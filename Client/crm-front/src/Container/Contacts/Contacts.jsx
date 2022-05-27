a5f0f50d57859a666e2aa6417aab5a4ce0c30561
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-05-26 21:29:38 -0500

    Updated components

--- Client/crm-front/src/Container/Contacts/Contacts.jsx
+++ Client/crm-front/src/Container/Contacts/Contacts.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './Contacts.scss';
 
+import * as AiIcons from 'react-icons/ai';
+import * as MdIcons from 'react-icons/md';
+
+
 const Contacts = () => {
   return (
     <div className='app__contacts'>
@@ -11,10 +15,10 @@ const Contacts = () => {
 
           <div className='app__contacts-panel'>
             {
-              [{ name: 'Mike', age: 24 }, { name: 'Andrew', age: 21 }, { name: 'Carlos', age: 28 }].map( (item, index) => (
+              [{ name: 'Mike Wolfenstein', age: 24 }, { name: 'Andrew Serrano', age: 21 }, { name: 'Carlos Windsenberg', age: 28 }].map( (item, index) => (
                 <div key={index} className= 'app__contact-detailed-info'>
-                    <h4> This client's name is { item.name } </h4>
-                    <p> His age is: { item.age } </p>
+                    <h4 className=''> { item.name } <span className='app__contact-icons'> <AiIcons.AiFillEdit /> <MdIcons.MdDelete /></span>  </h4>
+                    <p className=''> Age : { item.age } </p>
                 </div>
               ))
             }
